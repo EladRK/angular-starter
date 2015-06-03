d680c20ef3afa204894b41233a852ff93a46703e
Elad Katz <eladkt@gmail.com>
2015-06-03 05:06:14 +0300

    trying to make tsc --out to work correctly

---
+++ app.ts
@@ -0,0 +1,19 @@
+import {Component, View, bootstrap} from 'angular2/angular2';
+
+
+@Component({
+  selector: 'my-app'
+})
+@View({
+  template: '<h1>Hello {{ name }}</h1>'
+})
+// Component controller
+class MyAppComponent {
+  name: string;
+  
+  constructor() {
+    this.name = 'Alice';
+  }
+}    
+
+bootstrap(MyAppComponent); 
---
+++ public/theApp.js
@@ -0,0 +1,1 @@
+//# sourceMappingURL=theApp.js.map
