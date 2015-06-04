4dd92a64f801ecec96381a8aca9309803a49914b
Dima Kuzmich <dima.kuzmich.work@gmail.com>
2015-06-04 09:03:09 +0300

    build with maps

--- gulpfile.js
+++ gulpfile.js
@@ -3,28 +3,29 @@ var ts = require('gulp-typescript');
 var typescript = require('typescript');
 var del = require('del');
 var runSequence = require('run-sequence');
+var sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('default', function(callback){
-	runSequence(
-		'clean','ts', 'copy');
+gulp.task('default', function (callback) {
+	runSequence('clean', 'ts', 'copy');
 });
 
-gulp.task('ts', function() {
+gulp.task('ts', function () {
 	var tsProject = ts.createProject('tsconfig.json', {
-	    typescript: typescript
+		typescript: typescript
 	});
-	
+
 	return gulp.src(['typings/**/**.ts', 'src/**/**.ts'])
+		.pipe(sourcemaps.init())
         .pipe(ts(tsProject))
+		.pipe(sourcemaps.write('../maps', {includeContent: false, sourceRoot: '/scripts/src'}))
         .pipe(gulp.dest('build'));
 });
 
-gulp.task('copy', function(){
-	return gulp.src(['build/**/*.*', 'src/**/*.*'], {base: "."})
-	 	.pipe(gulp.dest('public/scripts'));
+gulp.task('copy', function () {
+	return gulp.src(['build/**/*.*', 'src/**/*.*', 'maps/**/*.*'], { base: "." })
+		.pipe(gulp.dest('public/scripts'));
 });
 
 gulp.task('clean', function () {
-	del('build');
-	del('public/scripts');
+	del(['build', 'maps','public/scripts']);
 });
