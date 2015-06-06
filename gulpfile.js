713b9ba80a6c00d4f227a63a7d59b64abc530ea3
Dima Kuzmich <dima.kuzmich.work@gmail.com>
2015-06-06 12:09:51 +0300

    Separated gulp tasks for cleaning of files. Added clean of src script directory after build. Added comments into gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -6,9 +6,10 @@ var runSequence = require('run-sequence');
 var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('default', function (callback) {
-	runSequence('clean', 'ts', 'html', 'copy');
+	return runSequence('clean-all', 'ts', 'html', 'copy', 'clean-src');
 });
 
+// compiles *.ts files by tsconfig.json file and creates sourcemap filse
 gulp.task('ts', function () {
 	var tsProject = ts.createProject('tsconfig.json', {
 		typescript: typescript
@@ -21,16 +22,34 @@ gulp.task('ts', function () {
         .pipe(gulp.dest('scripts/build'));
 });
 
+// copy *.html files (templates of components)
+// to apropriate directory under public/scripts
 gulp.task('html', function () {
 	return gulp.src(['scripts/src/**/**.html'])
         .pipe(gulp.dest('scripts/build'));
 });
 
+// copy generated/compiled files 
+// from scripts/ directory to public/scripts directory
 gulp.task('copy', function () {
 	return gulp.src(['scripts/**/*.*'], { base: "." })
 		.pipe(gulp.dest('public'));
 });
 
-gulp.task('clean', function () {
-	del(['scripts/build', 'scripts/maps','public/scripts']);
+//  clean all generated/compiled files 
+//	in both scripts/ and public/scripts/ directories
+gulp.task('clean-all', function () {
+	return runSequence('clean-src', 'clean-public');
+});
+
+//  clean all generated/compiled files 
+//	only in public/scripts/ directory
+gulp.task('clean-public', function () {
+	return del(['public/scripts']);
+});
+
+//  clean all generated/compiled files 
+//	only in both scripts/ directory
+gulp.task('clean-src', function () {
+	return del(['scripts/build', 'scripts/maps']);
 });
